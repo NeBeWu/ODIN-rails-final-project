78abada9bf7c3bb728dc1747a89a0896c420eb0a
NeBeWu <nebewu@gmail.com>
2022-08-04 22:37:47 -0300

    Add user model validations and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should validate_length_of(:password).is_at_least(10) }
+  end
 end
